3bd572f91b38ab80afd063e92aeb5d921d7f0a37
Gabriella <meow@hehe.moe>
2025-01-10 02:41:42 +0100

    Small overhaul

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-import { Wiimote } from './wii.js'
+import { EventEmitter } from 'events'
+import HID from 'node-hid'
 
-const wm = new Wiimote()
+export class Wiimote extends EventEmitter {
+    constructor() {
+        super()
+        this.wiimote = null
+        
+        while (!this.wiimote) {
+            let res = HID.devices().find(x => x.product.includes('Nintendo RVL-CNT-01'))
+            if (res) this.wiimote = new HID.HID(res.path)
+        }
 
-wm.setLeds(true, true, true, true)
+        this.emit('connected')
 
-wm.on('data', (data) => {
-    console.clear()
-    console.log(data)
-})
+        // Enable continuous data sending in mode 0x31 (Core Buttons and Accelerometer)
+        this.wiimote.write(Buffer.from([0x12, 0x04, 0x31]))
 
-wm.on('error', (err) => {
-    console.error('Error:', err)
-})
+        this.wiimote.on('data', this.onData.bind(this))
+        this.wiimote.on('error', this.onError.bind(this))
+    }
+
+    onData(buffer) {
+        if (buffer.length < 22) return console.error('Unexpected buffer length:', buffer.length)
+
+        this.emit('data', {
+            A:      (buffer[2] & 0x08) != 0, // Byte 2, bit 3
+            B:      (buffer[2] & 0x04) != 0, // Byte 2, bit 2
+            Plus:   (buffer[1] & 0x10) != 0, // Byte 1, bit 4
+            Minus:  (buffer[2] & 0x10) != 0, // Byte 2, bit 4
+            Home:   (buffer[2] & 0x80) != 0, // Byte 2, bit 7
+            Up:     (buffer[1] & 0x08) != 0, // Byte 1, bit 3
+            Down:   (buffer[1] & 0x04) != 0, // Byte 1, bit 2
+            Left:   (buffer[1] & 0x01) != 0, // Byte 1, bit 0
+            Right:  (buffer[1] & 0x02) != 0, // Byte 1, bit 1
+            One:    (buffer[2] & 0x02) != 0, // Byte 2, bit 1
+            Two:    (buffer[2] & 0x01) != 0, // Byte 2, bit 0
+            accelX: (buffer[3] - 128) / 128, // Byte 3
+            accelY: (buffer[4] - 128) / 128, // Byte 4
+            accelZ: (buffer[5] - 128) / 128, // Byte 5
+        })
+    }
+
+    onError(error) {
+        this.emit('error', error)
+    }
+
+    setLeds(one = false, two = false, three = false, four = false) {
+        this.wiimote.write(Buffer.from([0x11, (one * 0x10 + two * 0x20 + three * 0x40 + four * 0x80)]))
+    }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,25 @@
 # node-wiimote
+
+A super simple package for interacting with Wiimotes easily.
+
+## Accessible data
+* All buttons except the power button
+* Accelerometer data
+
+### Example code
+```js
+import { Wiimote } from 'node-wiimote';
+
+const wiimote = new Wiimote();
+
+// Turn all four leds on
+wiimote.setLeds(true, true, true, true);
+
+wiimote.on('data', (data) => {
+    // Handle data
+});
+
+wiimote.on('error', (error) => {
+    // Handle error
+});
+```
